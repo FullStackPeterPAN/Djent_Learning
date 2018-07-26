f3bfd7135186129c9769c96ab87a67ae707725ea
Yangjiangdai Pan <35268135+SAKURA-AYANE@users.noreply.github.com>
2018-07-26 18:48:04 +0100

    new classes

--- codes/read_audio.py
+++ codes/read_audio.py
@@ -1,24 +1,39 @@
 import wave
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-import scipy.io as sio
 import numpy as np
-import os
 
-x = np.random.randint(10, size=[1, 12])
-y = np.random.randint(10, size=[1, 12])
-model = Sequential()
-model.add(Dense(2, input_dim=12, activation='relu'))
-model.add(Dense(12, activation='sigmoid'))
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])  # compile the model
-
-# train the model
-model.fit(x, y, epochs=1, batch_size=1)
+dim = 0
+input_path = "F:/WorkSpace/DL_Sound_Effect/codes/data/train/input/"
+output_path = "F:/WorkSpace/DL_Sound_Effect/codes/data/train/output/"
+expected_path = "F:/WorkSpace/DL_Sound_Effect/codes/data/train/expected/"
 
 
-# evaluate the model
-loss, accuracy = model.evaluate(x, y, batch_size=1)
+# read input file
+def read_file(path, i):
+    input_filepath = path
+    input_filename = None
+    if path == input_path:
+        input_filename = "clean_" + str(i) + ".wav"
+    else:
+        input_filename = "tokyo_drive_" + str(i) + ".wav"
+    input_file = wave.open(input_filepath + input_filename, 'rb')
+    num_frame = input_file.getnframes()  # get the number of frames
 
-print(x)
-print(y)
-print(loss, accuracy)
+    # count frames number if it is clean audio
+    global frames
+    frames = num_frame
+    num_channel = input_file.getnchannels()  # get the number of channels
+    frame_rate = input_file.getframerate()  # get the rate of frames
+    num_sample_width = input_file.getsampwidth()  # get the width of sample
+    str_data = input_file.readframes(num_frame)  # read all frames
+    input_file.close()  # close the file
+    wave_data = np.fromstring(str_data, dtype=np.short)  # turn the data to numpy array
+    wave_data.shape = -1, num_channel  # shape the data depending on the number of channels
+    wave_data = wave_data.T  # turn the wave data
+    wave_data = wave_data
+    global dim
+    dim = len(wave_data[0])
+    return wave_data  # return numpy data
+
+
+def get_dim():
+    return dim
---
+++ codes/test_with_audio.py
@@ -0,0 +1,12 @@
+# import necessary stuff
+
+import wave
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.models import load_model
+import scipy.io as sio
+import numpy as np
+import os
+
+model_path = "F:/WorkSpace/DL_Sound_Effect/codes/data/model/model.h5"
+model = load_model(model_path)
